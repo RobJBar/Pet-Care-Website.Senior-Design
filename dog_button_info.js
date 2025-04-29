fa2de498e9fbf35c7cbaf1dca224e10d1025f244
Robyn <robynjaebar@gmail.com>
2025-04-28 22:24:09 -0500

    New changes

---
+++ dog_button_info.js
@@ -0,0 +1,245 @@
+function French_Bulldog_Name() {
+ document.getElementById("Dog_Name").innerHTML = "French Bulldog";
+}
+function French_Bulldog() {
+ document.getElementById("Dog").innerHTML = `<img src='French_Bulldog.jpg' alt='French Bulldog'>
+ <p><b>General Info: </b>The French Bulldog is a smaller breed of the bulldog,
+with 1 noticeable difference being that it has upright ears. It tends to be
+around 11 to 13 inches, and weighs from 17 to 24 pounds if female and 20 to 28
+pounds if male.</p>
+ <p><b>Temperament: </b>This dog is very friendly with people, including
+children and strangers. It also gets along well with other dogs. This dog is good
+for people with large families or if they want several dogs. French bulldogs are
+fairly quiet and do not bark very often.</p>
+ <p><b>Exercise: </b>French bulldogs do not require very much outdoor
+exercise. They only require a small daily walk to stay healthy. The dense build
+of these dogs make it difficult for them to swim, so they should not be allowed
+to swim in deep water.</p>
+ <p><b>Common Health Issues: </b>This breed of dog tends to have many health
+issues, 1 of the biggest ones coming from its flat face. French bulldogs
+typically have breathing problems, causing it to become exerted quickly, as well
+as having loud breathing, so it is not recommended to exercise these dogs for a
+long time. They also tend to have skin allergies, causing itching and infections.
+It is possible for French bulldogs to develop hip dysplasia, where the hip joint
+develops incorrectly, causing pain and difficulty moving.</p>`;
+}
+function Labrador_Retriever_Name() {
+ document.getElementById("Dog_Name").innerHTML = "Labrador Retriever";
+}
+function Labrador_Retriever() {
+ document.getElementById("Dog").innerHTML = `<img src='Labrador_Retriever.jpg' alt='Labrador Retriever'>
+ <p><b>General Info: </b>The Labrador Retriever is a large breed of dog, with
+a height ranging from 22 to 24 inches tall and weighing from 55 to 70 pounds if
+female and 65 to 80 pounds if male.</p>
+ <p><b>Temperament: </b>Labradors are well known for being extremely friendly
+with everyone. They will get along very well with both people and other dogs.
+These dogs have a lot of energy and require lots of attention.</p>
+ <p><b>Exercise: </b>Labradors require a lot of exercise to stay healthy and
+happy. These dogs require at least an hour of exercise a day. It is also healthy
+for them to get a variety of exercises, such as going on walks, swimming, or
+playing fetch.</p>
+ <p><b>Common Health Issues: </b>Labradors have a possibility to develop hip
+and elbow dysplasia. Their hip and elbow joints have a possibility to develop
+incorrectly causing pain and difficulty moving. They typically want to eat a lot
+of food, and very quickly, meaning they could develop obesity. Their large ears
+make them more likely to develop ear infections.</p>`;
+}
+function Golden_Retriever_Name() {
+ document.getElementById("Dog_Name").innerHTML = "Golden Retriever";
+}
+function Golden_Retriever() {
+ document.getElementById("Dog").innerHTML = `<img src='Golden_Retriever.jpg' alt='Golden Retriever'>
+ <p><b>General Info: </b>The Golden Retriever is a large breed of dog, with a
+height ranging from 21 to 24 inches and weighing from 55 to 65 pounds if female
+and 65 to 70 pounds if female.</p>
+ <p><b>Temperament: </b>Golden Retrievers are similar to Labradors as they are
+extremely friendly, and are able to get along with practically any dog or person.
+They are playful and need a lot of attention to stay mentally healthy.</p>
+ <p><b>Exercise: </b>Golden Retrievers need at least an hour of exercise
+everyday for their health. Running, biking, fetching, and swimming are healthy
+for Golden Retrievers, but these dogs also enjoy certain types of work. Golden
+Retrievers are good hunters, and they can be exercised by aiding in hunts.</p>
+ <p><b>Common Health Issue: </b>Golden Retrievers have a possibility of
+developing hip and elbow dysplasia at a young age, which causes pain and
+difficulty moving. They are likely to develop allergies, which can cause skin
+irritations and respiratory problems. Golden Retrievers have a higher chance of
+developing certain types of cancer, such as lymphoma.</p>`;
+}
+function German_Shepherd_Name() {
+ document.getElementById("Dog_Name").innerHTML = "German Shepherd";
+}
+function German_Shepherd() {
+ document.getElementById("Dog").innerHTML = `<img src='German_Shepherd.jpg' alt='German Shepherd'>
+ <p><b>General Info: </b>The German Shepherd is a large breed of dog, with a
+height ranging from 22 to 26 inches and weighing from 50 to 70 pounds if it is
+female and from 65 to 90 if male.</p>
+ <p><b>Temperament: </b>These dogs can require some time to get along with a
+new owner, but once it has, it is very loyal and friendly to them, though they
+will struggle to get along with strangers and other dogs. They are easily trained
+which makes them a popular choices for people who need a work dog, or just a dog
+that can do tricks.</p>
+ <p><b>Exercise: </b>German Shepherds require 1 to 2 hours of exercise every
+day. They can be exercised by running and swimming, or biking with them to allow
+them to run at a higher speed than just running would allow you, as German
+Shepherds are incredibly fast. German Shepherds were bred to be herding dogs, so
+if you can find herding exercises for your German Shepherd, this can help their
+physical health and greatly help their mental health, allowing them to engage in
+their natural instincts.</p>
+ <p><b>Common Health Issue: </b>German Shepherds have a possibility of
+developing hip and elbow dysplasia at a young age, which causes pain and
+difficulty moving. They also have a fairly common chance of developing
+“Degenerative Myelopathy,” which is a disease that slowly affects the spinal cord
+and hinders their ability to use their hind legs until it eventually can’t move
+them at all</p>`;
+}
+function Poodle_Name() {
+ document.getElementById("Dog_Name").innerHTML = "Poodle";
+}
+function Poodle() {
+ document.getElementById("Dog").innerHTML = `<img src='Poodle.jpg' alt='Poodle'>
+ <p><b>General Info: </b>The standard Poodle is a medium large dog, with a
+height ranging from 18 to 24 inches and weighing from 40 to 50 pounds if female
+and 60 to 70 pounds if male.</p>
+ <p><b>Temperament: </b>Poodles are very friendly with other people, including
+children and strangers, and get along fairly well with other dogs. They are
+typically high energy, but very obedient and well behaved dogs, so long as they
+remain mentally stimulated.</p>
+ <p><b>Exercise: </b>Poodles require about 1 hour of exercise per day. Many of
+them love water, so swimming is typically a good exercise. They also enjoy
+playing fetch and going on walks. Obedience training can also be good to give
+Poodles mental stimulation.</p>
+ <p><b>Common Health Issue: </b>Poodles have a possibility of developing hip
+and elbow dysplasia at a young age, which causes pain and difficulty moving. They
+can also develop luxating patella, which causes the kneecap to disclocate. They
+also have a chance of developing hypothyroidism, which means they cannot produce
+enough thyroid hormones, which can cause increased weight gain, even if their
+diet is decreased. It also can cause skin dryness, hair loss, and cold
+intolerance. </p>`;
+}
+function Dachshund_Name() {
+ document.getElementById("Dog_Name").innerHTML = "Dacshund";
+}
+function Dachshund() {
+ document.getElementById("Dog").innerHTML = `<img src='Dacshund.jpg' alt='Dacshund'>
+ <p><b>General Info: </b>Standard Dachshunds are a small breed of dog, with
+both male and female dogs having a weight ranging from 16 to 32 pounds and have a
+size ranging from 8 to 9 inches tall. It is also known as the weiner dog due to
+its long body compared to its height.</p>
+ <p><b>Temperament: </b>The Dachshund gets along fairly well with their
+family, but their history as popular hunting dogs can make it difficult for them
+to get along with children unless they are trained to interact with them. They
+can get along with other dogs and strangers, but if not trained properly, they
+can be aggressive with them. It is a stubborn breed, so it can be hard to
+train.</p>
+ <p><b>Exercise: </b>Dachshunds aren’t a very active breed, only requiring 30
+minutes to an hour of exercise a day. They aren’t built well for many types of
+different exercises, so just walking or playing fetch with them. It is important
+to not overexert themselves as this can cause back problems, especially if it
+involves a lot of jumping.</p>
+ <p><b>Common Health Issue: </b>One of the most common health issues for
+Dachshunds is Intervertebral Disc Disease (IVDD) which is where the discs in the
+spine degrade, which causes mobility issues and pain, and in worse cases can
+cause paralysis. They can also develop eye issues, like cataracts. All small dogs
+have a higher chance of developing a Luxating Patella, which causes the knee to
+dislocate, which can make it difficult for them to walk.</p>`;
+}
+function Bulldog_Name() {
+ document.getElementById("Dog_Name").innerHTML = "Bulldog";
+}
+function Bulldog() {
+ document.getElementById("Dog").innerHTML = `<img src='Bulldog.jpg' alt='Bulldog'>
+ <p><b>General Info: </b>Bulldogs are a medium breed of dog, with a height
+ranging from 14 to 15 inches and weighing from 50 to 55 pounds if male and 40 to
+50 pounds if female.</p>
+ <p><b>Temperament: </b>Bulldogs are typically a very laid back breed of dog.
+When not exercising, these dogs like to relax inside. Bulldogs are typically very
+stubborn, and unlike many other dogs, they aren’t considered very intelligent, so
+training can be difficult.</p>
+ <p><b>Exercise: </b>Bulldogs aren’t incredibly athletic dogs and only require
+30 minutes to 40 minutes of exercise per day, but their low energy means that
+their exercise should be more spread throughout the day. They prefer low impact
+exercises such as walks or fetching. Bulldogs are not made for swimming, so they
+should not be allowed to swim in deep waters, but allowing them to be in a
+shallow pool of water will allow them to exercise while staying cool.</p>
+ <p><b>Common Health Issue: </b>When Bulldogs are young, they have a chance to
+develop hip dysplasia, where the hip joint develops incorrectly, causing pain and
+difficulty moving. Their flat face has a very high likelihood of developing
+Brachycephalic Airway Syndrome (BOAS), which narrows their airways making it
+difficult for them to breath, especially when they are exercising and when they
+are in hot weather. Their skin folds can trap moisture in the folds, which can
+cause infections and skin irritation. Infections can cause conjunctivitis, also
+known as pink eye, which causes inflammation on the eye.</p>`;
+}
+function Beagle_Name() {
+ document.getElementById("Dog_Name").innerHTML = "Beagle";
+}
+function Beagle() {
+ document.getElementById("Dog").innerHTML = `<img src='Beagle.jpg' alt='Beagle'>
+ <p><b>General Info: </b>Beagles are a small breed of dog, with a height
+ranging from 13 to 15 inches and weighing from 22 to 25 pounds if male and 20 to
+23 pounds if female.</p>
+ <p><b>Temperament: </b>Beagles get along well with people and family, as well
+as other dogs. They are typically high energy and love to play. They love
+affection, so it is important to be able to always give them affection. Their
+behavior makes them good family pets.</p>
+ <p><b>Exercise: </b>Beagles require an hour to 90 minutes of exercise a day.
+They are naturally pack animals, so it is healthy for an owner to be a part of
+their exercise as much as possible. These dogs enjoy walks, playing fetch, and
+swimming. They do not enjoy leashes, and prefer to be able to run around without
+one, so it is good to have an outdoor area where they can run around without a
+leash. They like digging holes, so an owner should keep a close eye on their
+Beagle while outside.</p>
+ <p><b>Common Health Issue: </b>Beagles have a possibility of developing hip
+dysplasia at a young age, which causes pain and difficulty moving. They have a
+higher chance of being born with epilepsy, especially if one of the previous
+parents had epilepsy. Beagles also have a chance of being born with dwarfism,
+which causes their legs to be incredibly short and their intervertebral discs to
+degrade, which can cause movement issues.</p>`;
+}
+function Rottweiler_Name() {
+ document.getElementById("Dog_Name").innerHTML = "Rottweiler";
+}
+function Rottweiler() {
+ document.getElementById("Dog").innerHTML = `<img src='Rottweiler.jpg' alt='Rottweiler'>
+ <p><b>General Info: </b>Rottweilers are a giant breed of dog, with a height
+ranging from 22 to 27 inches and weighing from 95 to 135 pounds if male and 80 to
+100 pounds if female.</p>
+ <p><b>Temperament: </b>Despite their large size, Rottweilers are considered
+to be very gentle and protective of their owners and family. If they are raised
+incorrectly, they can develop bad habits, so Rottweilers are better for more
+experienced dog owners. They are great at learning and following commands, so
+they are common work dogs.</p>
+ <p><b>Exercise: </b>Rottweilers require 1 to 2 hours of exercise a day. They
+enjoy exercises such as walks and swimming, especially when their owners
+participate with them. They are also work dogs, so if possible, their owners can
+give them tasks like herding, tracking, or carting for exercise.</p>
+ <p><b>Common Health Issue: </b>Rottweilers have a higher possibility of
+developing cancer than most dogs, including bone cancer. They have a possibility
+of developing hip and elbow dysplasia at a young age, which causes pain and
+difficulty moving. Rottweilers can also develop arthritis and bloating. They can
+also develop eye problems such as cataracts, which is when the eye lens to cloud,
+causing vision problems.</p>`;
+}
+function German_Shorthaired_Pointer_Name() {
+ document.getElementById("Dog_Name").innerHTML = "German Shorthaired Pointer";
+}
+function German_Shorthaired_Pointer() {
+ document.getElementById("Dog").innerHTML = `<img src='German_Shorthaired_Pointer.jpg' alt='German_Shorthaired_Pointer'>
+ <p><b>General Info: </b>German Shorthaired Pointers are a large breed of dog,
+with a height ranging from 21 to 25 inches and weighing from 55 to 70 pounds if
+male and 45 to 60 pounds if female.</p>
+ <p><b>Temperament: </b>German Shorthaired Pointers are typically very
+friendly with their owners, family, and children. They are incredibly
+affectionate, and require a lot of attention. They are known for having a large
+amount of energy that makes them difficult to deal with if that energy isn’t
+expended.</p>
+ <p><b>Exercise: </b>German Shorthaired Pointers require 1 to 2 hours of
+exercise a day, and their high energy means these hours should have them be as
+active as possible. They enjoy running, swimming, and dock diving, allowing for
+them to burn their high amount of energy.</p>
+ <p><b>Common Health Issue: </b>German Shorthaired Pointers have a possibility
+of developing hip dysplasia at a young age, which causes pain and difficulty
+moving. They have a higher likelihood of developing certain eye issues, like
+entropion, which causes their eyelids to fold inwards, which will cause eye pain
+from the eyelash constantly rubbing against the cornea.</p>`;
+}
