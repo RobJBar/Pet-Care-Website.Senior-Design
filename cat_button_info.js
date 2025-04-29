d92f2cdefe8b4b518d31d9eff296c86114cfc277
Robyn <robynjaebar@gmail.com>
2025-04-28 22:19:53 -0500

    Changes made

---
+++ cat_button_info.js
@@ -0,0 +1,120 @@
+function Siamese_Name() {
+    document.getElementById("Cat_Name").innerHTML = "Siamese";
+  }
+  
+  function Siamese() {
+    document.getElementById("Cat").innerHTML = `<img src="Siamese.jpg" alt="Siamese">
+    <p><b>General Info: </b>Siamese cats are a medium breed of cat, weighing from 8 to 12 pounds. They are typically 8 to 10 inches tall.</p>
+    <p><b>Temperament: </b>Siamese cats are known for being one of the most intelligent breeds of cat. They are typically very friendly towards their owner and are more social than most other cats, so they can bond with their owner very quickly. Their intelligence makes them able to learn good behaviors quickly, and enjoy anything that stimulates their mind. They can become depressed and display bad behaviors if they don’t get a lot of attention or are treated with negative treatment instead of positive reinforcement.</p>
+    <p><b>Activity: </b>Siamese cats require a large amount of attention from their owners and will typically follow them around and meow loudly. They enjoy being pet and it is an easy way to give them their required attention. Even with their higher love of attention compared to other cats, they still need to be given space when they need it. Siamese cats enjoy toys that allow them to use their brain, but they can get bored of these toys if they play with them for too long, so it is healthy to have a rotation of toys for them to play with. They can also be entertained with puzzle feeders, which will exercise their brain while also slowing down how fast they eat. These also need to be switched regularly.</p>
+    <p><b>Common Health Issue: </b>One of the most common health issues in Siamese cats is amyloidosis, which affects the liver, causing protein deposits to build up in organs, and can cause organ failure in many cases. They also have a higher chance of being born with Progressive Retinal Atrophy, which is a hereditary disease that slowly causes the retinas to degenerate and causes vision problems, and eventually blindness. They can develop dental diseases, which can harm their teeth.</p>`;
+  }
+  
+  function Persian_Name() {
+    document.getElementById("Cat_Name").innerHTML = "Persian";
+  }
+  
+  function Persian() {
+    document.getElementById("Cat").innerHTML = `<img src="Persian.jpg" alt="Persian">
+    <p><b>General Info: </b>Persian cats are a medium breed of cat, weighing from 7 to 12 pounds. They are typically 10 to 15 inches tall.</p>
+    <p><b>Temperament: </b>Persian cats are usually very quiet and calm animals, and aren’t fond of loud environments. They are calm and unlikely to scratch their owners even when aggravated, so it is important for owners to make sure they aren’t irritating their cat. It is important to give them a place to go to if they need to be alone, in case there are new or loud people in the house.</p>
+    <p><b>Activity: </b>Persian cats do not require too much attention, and as long as the owner plays with them fairly often, they will behave properly. They also need to have their fur groomed properly and daily to deal with shedding and keeping their fur tamed. They should be exclusively indoor cats, as they are not able to deal with other aggressive animals and their coat can easily collect dirt from outside.</p>
+    <p><b>Common Health Issue: </b>Persian cats are prone to dental diseases, such as when food and bacteria build up in the gums, so their teeth should be checked regularly. They are also more likely to have eye problems, especially due to their flat faces making them more likely to get the cornea of their eyes grazed. This can lead to difficulty seeing and even possibly going blind. They are more likely to have liver problems, like Polycystic Kidney Disease, which is a hereditary condition that causes cysts to develop in the kidney, and can lead to kidney failure.</p>`;
+  }
+  
+  function Maine_Coon_Name() {
+    document.getElementById("Cat_Name").innerHTML = "Maine Coon";
+  }
+  
+  function Maine_Coon() {
+    document.getElementById("Cat").innerHTML = `<img src="Maine_Coon.jpg" alt="Maine Coon">
+    <p><b>General Info: </b>Maine Coons are a large breed of cat weighing between 15 to 25 pounds. They are typically 10 to 16 inches tall.</p>
+    <p><b>Temperament: </b>Maine Coons are very affectionate pets that usually make great family pets. Their loyalty and social nature are traits that many people consider to be similar to dogs. They are intelligent and gentle, meaning they can get along with other cats, and even friendly dogs. They enjoy attention despite the fact that they don’t always want physical affection. They are fairly independent, but can still become lonely if left alone.</p>
+    <p><b>Activity: </b>Unlike many cats, Maine Coons should have some level of outside access to allow them to exercise, as they typically know how much exercise they need, and will exercise on their own. Maine Coons greatly enjoy water and like to play and swim in it. They are quick learners and enjoy playing games with their owners. Like most cats, their activity will come in short bursts and they will spend the rest of their time relaxing, so owners should not try to make them exercise or play outside of these bursts. They enjoy it when they have people or other cats to play with. They sleep more than the average cat breed, and they should be given that time to sleep.</p>
+    <p><b>Common Health Issue: </b>Maine Coons have a possibility to develop hip dysplasia while they are young. This is when the hip joint does not develop properly causing movement to become difficult and painful. The fur around their ears can cause moisture and dirt to become trapped, leading to ear infections. They have an increased risk of Hypertrophic cardiomyopathy, which causes the muscle tissue of the heart to thicken, making it harder to pump blood. While still fairly rare, Maine Coons have a higher chance to develop Spinal Muscular Atrophy, which weakens the muscles and can make it very difficult to move.</p>`;
+  }
+  
+  function Ragdoll_Name() {
+    document.getElementById("Cat_Name").innerHTML = "Ragdoll";
+  }
+  
+  function Ragdoll() {
+    document.getElementById("Cat").innerHTML = `<img src="Ragdoll.jpg" alt="Ragdoll">
+    <p><b>General Info: </b>Ragdolls are a large breed of cat, with female cats weighing from 8 to 15 pounds and male cats weighing from 12 to 20 pounds. These cats have a height ranging from 9 to 11 inches tall.</p>
+    <p><b>Temperament: </b>Ragdoll cats are playful and very affectionate. Their love for affection will typically have them follow their owners around the house seeking attention, but if they want to be left alone, they should be given that space. They can tolerate a decent amount of noise, so this cat is good for busy families. They are a very calm and gentle breed of cat that greatly enjoys attention. They can get along with practically anyone or anything that acts friendly, even complete strangers.</p>
+    <p><b>Activity: </b>Ragdolls are a breed of cat that requires a large amount of attention to stay mentally healthy. While they can be left alone for short periods of time, it is best to have at least one person they can interact with at any time during the day. They respond well to positive praise and affection, so it should be given out frequently with good behavior. Playing games with them while also allowing them time to relax is very important for their health and bonding. They enjoy games that can simulate hunting, like fishing rod toys or toy mice.</p>
+    <p><b>Common Health Issue: </b>Ragdolls have an increased risk of Hypertrophic Cardiomyopathy, which causes the muscle tissue of the heart to thicken, making it harder to pump blood. They have a higher chance of developing urinary tract issues, which causes pain while urinating. It also has a higher chance of having Polycystic Kidney Disease, which is a genetic disease that causes cysts to develop in the kidney, and can cause kidney failure.</p>`;
+   }
+
+  function Bengal_Name() {
+  document.getElementById("Cat_Name").innerHTML = "Bengal";
+   }
+
+  function Bengal() {
+  document.getElementById("Cat").innerHTML = `<img src="Bengal.jpg" alt="Bengal">
+  <p><b>General Info: </b>Bengal cats are a medium breed of cat that usually weigh between 8 to 15 pounds. They have a height ranging from 8 to 10 inches.</p>
+  <p><b>Temperament: </b>Bengal cats are a highly active and adventurous breed of cat. This high energy has made many owners describe them as mischievous. Their behavior can make it difficult for them to be trained, but they are still intelligent enough where it won’t be impossible. They can be territorial with other cats, but if given the time to adjust to new cats in their territory, they will learn to get along with it.</p>
+  <p><b>Activity: </b>Bengal cats are highly adventurous and enjoy exploring their environment, so giving them a controlled area of outside access or walking them outside on a leash can be a good way of allowing them to explore. Their home inside should have plenty of activities for them to do and toys to play with. They enjoy climbing, so having a tall climbing tree is great not only for them but also their owners, since it will stop them from scratching and climbing their furniture. They enjoy playing games that involve pouncing on things like toys on string. Some Bengal cats enjoy shallow water so if they enjoy it, giving them a shallow pool of water.</p>
+  <p><b>Common Health Issue: </b>Bengal cats have an increased risk of Hypertrophic Cardiomyopathy, which causes the muscle tissue of the heart to thicken, making it harder to pump blood. They can develop Progressive Retinal Atrophy, which causes their eyesight to slowly degrade and eventually go blind. They have a higher chance of developing Pyruvate Kinase Deficiency which is an inherited disease that causes a lack of the enzyme pyruvate kinase, which causes harm to red blood cells and can cause anemia.</p>`;
+  }
+
+  function Abyssinian_Name() {
+  document.getElementById("Cat_Name").innerHTML = "Abyssinian";
+  }
+
+  function Abyssinian() {
+  document.getElementById("Cat").innerHTML = `<img src="Abyssinian.jpg" alt="Abyssinian">
+  <p><b>General Info: </b>Abyssinian cats are a medium breed of cat that usually weigh between 8 to 12 pounds. They have a height ranging from 8 to 10 inches.</p>
+  <p><b>Temperament: </b>Abyssinian cats are typically very smart and curious, but also can be cautious around new people. Even with their cautious nature, they will still get along with humans and even dogs well. They are incredibly social with both humans and animals, and greatly enjoy being around others. They aren’t usually lap cats, but they still enjoy the company of their owners.</p>
+  <p><b>Activity: </b>The curious nature of Abyssinian cats make them love exploring, so giving them a controlled outside area to explore is very healthy for them. They enjoy climbing trees or climbable scratching posts, as well as games and toys that are highly interactable. They will typically want their owners to use these toys and play with them. They can get bored of the same toys very quickly, so having a large variety of toys to play with.</p>
+  <p><b>Common Health Issue: </b>Abyssinian cats have a higher likelihood of developing Periodontal Disease, which is an infection around the gums which can lead to teeth loss and gum inflammation. The Abyssinian breed has a higher likelihood of inheriting Patellar Luxation, which causes the kneecap to dislocate, which can cause difficulty moving and pain. They have a higher likelihood of having Hyperthyroidism, which is an overactive thyroid, causing unwanted weight loss and an increased heart rate, among other things.</p>`;
+  }
+
+  function Birman_Name() {
+  document.getElementById("Cat_Name").innerHTML = "Birman";
+  }
+
+  function Birman() {
+  document.getElementById("Cat").innerHTML = `<img src="Birman.jpg" alt="Birman">
+  <p><b>General Info: </b>Birmans are a medium breed of cat that usually weigh between 8 to 12 pounds. They have a height ranging from 8 to 11 inches.</p>
+  <p><b>Temperament: </b>Birmans are highly social, affectionate, and gentle cats. They are popular among families with children. They are very loyal towards their owner and if they belong to a family, they will often pick a favorite who they will spend the most amount of time with and seek out the most. They aren’t highly active and enjoy physical affection and attention, so they almost always make excellent lap cats. While they are highly affectionate, they should still be given time and space to adjust to a new environment and people.</p>
+  <p><b>Activity: </b>Birmans enjoy playing games, but they do not have the same amount of hunting instincts that most other cats do, so intelligence games and training games are usually preferred instead of hunting games. They enjoy puzzle feeders, which will allow the cat to eat their food at a lower pace and exercise their brain. If they are not stimulated through toys and games, they can become lazy and inactive, so it is important for owners to take time out of their day to play with their cat.</p>
+  <p><b>Common Health Issue: </b>Birmans have a higher likelihood of inheriting Hemophilia, which harms the cat's ability to create blood clots, making wounds bleed for much longer, and can increase the chance for internal bleeding. They have a higher chance of having kidney problems, such as Polycystic Kidney Disease, which can cause kidney problems and eventually kidney failure. They are prone to dental diseases if their teeth aren’t cleaned regularly, and these diseases can cause pain when eating and gum inflammation.</p>`;
+  }
+
+  function Oriental_Shorthair_Name() {
+    document.getElementById("Cat_Name").innerHTML = "Oriental Shorthair";
+  }
+  
+  function Oriental_Shorthair() {
+    document.getElementById("Cat").innerHTML = `<img src="Oriental_Shorthair.jpg" alt="Oriental Shorthair">
+    <p><b>General Info: </b>The Oriental Shorthair are a medium breed of cat that usually weighs between 8 to 12 pounds. They have a height ranging from 9 to 11 inches.</p>
+    <p><b>Temperament: </b>Oriental Shorthair cats are an affectionate but sensitive breed of cats, so they might not be good cats for families with young children. They get along with people and animals that can respect their space, like other cats and older children. They are still social and enjoy following their owner around the house, and cuddling with their owner. They can be left alone for a short period of time, but can become lonely if they are left alone for longer periods of time.</p>
+    <p><b>Activity: </b>Oriental Shorthairs are fairly active cats that need their owners to spend a good amount of time every day to play with them. If an owner does not have as much time to play with their cat, then another cat can also be a good way for them to stay active. They enjoy following their owner around the house and being around people. They enjoy climbing and jumping from high places, so having a climbing tree for them can be great for their physical and mental health. Playing with them on a regular basis is also important for both their health and bonding.</p>
+    <p><b>Common Health Issue: </b>Oriental Shorthairs have a higher chance of inheriting Progressive Retinal Atrophy, which causes their eyesight to gradually degrade and can eventually lead to blindness. They are prone to Amyloidosis, which causes proteins to build up in the tissue and organ, which can lead to organ failure. They have a higher chance of developing Dilated Cardiomyopathy, which weakens the heart muscles, making pumping blood more difficult.</p>`;
+  }
+  
+  function Sphynx_Name() {
+    document.getElementById("Cat_Name").innerHTML = "Sphynx";
+  }
+  
+  function Sphynx() {
+    document.getElementById("Cat").innerHTML = `<img src="Sphynx.jpg" alt="Sphynx">
+    <p><b>General Info: </b>Sphynx cats are a medium breed of cat that usually weighs between 10 to 12 pounds. They have a height ranging from 8 to 10 inches. They are immediately recognizable by their lack of fur.</p>
+    <p><b>Temperament: </b>Sphynx cats are a very intelligent and curious breed of cat. They can be fairly mischievous with their high energy and constant want for attention. This mischievous nature can become destructive if they aren’t properly taken care of. They are highly social and get along with people and animals very well. They can be left alone for short periods of time, but will get lonely if left alone for longer periods. They are very loyal to their owners, and tend to follow them around the house.</p>
+    <p><b>Activity: </b>Because Sphynx cats are highly intelligent, having challenging games for them to play, as well as a consistent rotation of these toys. They greatly enjoy exploring their environment, so it is good to have a cat jungle gym for them to explore, or they might try to get in places they shouldn’t be, like their owners' cupboards. Because they are a hairless breed of cat, they get colder quicker and easier than most other cats, so it is good for owners to allow them to stay under their blankets with them, or allow them to cuddle with them for warmth. They can get lonely easily, so if an owner wants to make sure they always have company, it would be a good idea to have another Sphynx cat in the home for them to interact with.</p>
+    <p><b>Common Health Issue: </b>Because Sphynx cats have no fur, they are more susceptible to skin problems like rashes, fungal infections, and sunburns. They lack the nasal protection that comes from most cat’s fur, causing respiratory issues and infections, such as coughing or trouble breathing. They are more prone to Hypertrophic Cardiomyopathy, which is when the heart muscles thicken, making it more difficult for the heart to pump blood. They are more likely to have stomach issues, like Irritable Bowel Syndrome, which causes abdominal pain and diarrhea.</p>`;
+  }
+  
+  function Devon_Rex_Name() {
+    document.getElementById("Cat_Name").innerHTML = "Devon Rex";
+  }
+  
+  function Devon_Rex() {
+    document.getElementById("Cat").innerHTML = `<img src="Devon_Rex.jpg" alt="Devon Rex">
+    <p><b>General Info: </b>Devon Rex cats are a small breed of cat that usually weighs between 6 to 9 pounds. They have a height ranging from 10 to 12 inches.</p>
+    <p><b>Temperament: </b>Devon Rex cats are an incredibly active and social breed of cat that gets along with children and other animals very well. They are affectionate and love attention, with their personality often being compared to dogs. They will do pretty much anything they can to get attention from their owners. They are loyal to their owners and enjoy cuddling with them.</p>
+    <p><b>Activity: </b>Unlike most cats, Devon Rex cats enjoy exercise a lot more. Even though they usually want more exercise than most cats, Devon Rex cats get bored of these activities quickly, so their owner should make these exercises as quick and energetic as possible. After an owner plays with their cat, they should allow the cat time to relax and sleep. They are intelligent, so they should have a constant rotation of toys to play with when they get bored. They enjoy climbing things, so having a climbing tree is a good toy for them to have. Devon Rex cats can even learn how to play fetch, and can greatly enjoy it once they are taught.</p>
+    <p><b>Common Health Issue: </b>Devon Rex cats have a higher chance of developing hip dysplasia when they are born or as kittens. Hip dysplasia is when the hip joint does not develop properly causing difficulty and pain while moving. They can also inherit Hereditary Myopathy, which causes the muscles in the body to weaken. They have a higher chance of having Hypertrophic Cardiomyopathy, which is when the muscles in the heart thicken, making it more difficult for the heart to pump blood.</p>`;
+  }
+  
